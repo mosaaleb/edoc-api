4a9bf532322c64af38da3e50f6bc57aae4f10f21
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-17 23:02:35 +0200

    Add validations to account email and specs

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,7 +3,7 @@
 class Account < ApplicationRecord
   has_secure_password
 
-  validates :email,
-            :first_name,
-            :last_name, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,6 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+  it { is_expected.to validate_uniqueness_of(:email) }
+  it { is_expected.to allow_value('email@address.foo').for(:email) }
+  it { is_expected.not_to allow_value('foo').for(:email) }
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_presence_of(:first_name) }
   it { is_expected.to validate_presence_of(:last_name) }
