20ed19b9eacc3b70bab656cd4772e049e0f5a4b5
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-19 01:03:39 +0200

    add account validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,4 +6,12 @@ class Account < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true,
                     format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
+
+  validates :password, length: { minimum: 6, maximum: 30 }
+  validates :first_name, :last_name, length: { minimum: 2, maximum: 20 }
+
+  # instance methods
+  def name_and_id
+    as_json(only: %i[id first_name last_name])
+  end
 end
--- test/factories/accounts.rb
+++ test/factories/accounts.rb
@@ -3,8 +3,8 @@
 FactoryBot.define do
   factory :account do
     email { Faker::Internet.unique.email }
-    first_name { Faker::Name.name }
-    last_name { Faker::Name.name }
+    first_name { Faker::Name.name[2..20] }
+    last_name { Faker::Name.name[2..20] }
     password { Faker::Internet.password }
   end
 end
