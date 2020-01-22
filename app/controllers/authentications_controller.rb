56af9eef9d052b644593b5d720e36409c1b7b032
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-22 22:09:02 +0200

    refactor authentication controller: - find role associated with the account - send data back

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -9,7 +9,9 @@ class AuthenticationsController < ApplicationController
 
     res = {
       auth_token: auth_token,
-      current_user: account.id_and_name,
+      current_user: account_role
+          .as_json(only: [:id],
+                   include: { account: { only: %i[first_name last_name] } }),
       message: 'You have successfully signed in.'
     }
     render json: res
@@ -23,5 +25,9 @@ class AuthenticationsController < ApplicationController
 
   def account
     Account.find_by(email: auth_params[:email])
+  end
+
+  def account_role
+    Patient.find(account.role_id)
   end
 end
