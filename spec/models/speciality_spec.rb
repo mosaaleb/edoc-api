e2cc1429283750dd5e2bde2f356f1938273830d6
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-24 23:24:00 +0200

    add speciality model specs

--- spec/models/speciality_spec.rb
+++ spec/models/speciality_spec.rb
@@ -3,6 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Speciality, type: :model do
-  it { is_expected.to have_many(:doctors) }
-  it { is_expected.to validate_presence_of(:name) }
+  describe 'associations & validations' do
+    it { is_expected.to have_many(:doctors) }
+    it { is_expected.to validate_presence_of(:speciality) }
+  end
+
+  describe '::special_in' do
+    let(:speciality) { create(:speciality) }
+
+    it 'returns all specialities by their name' do
+      spec = described_class.special_in(speciality.speciality)
+      expect(spec.size).to eq 1
+    end
+  end
 end
