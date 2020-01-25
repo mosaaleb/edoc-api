9d1b53ed07a0618da34eedd4ca9f0939e295c903
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-01-25 17:08:38 +0200

    likes association tests for patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,9 @@
 class Patient < ApplicationRecord
   has_one :account, as: :role, dependent: :destroy
   has_many :appointments, dependent: :destroy
-  has_many :doctors, through: :appointments
+  has_many :appointed_doctors, through: :appointments, source: :doctor
+  has_many :doctor_likes, dependent: :destroy
+  has_many :liked_doctors, through: :doctor_likes, source: :doctor
 
   accepts_nested_attributes_for :account
 
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -6,7 +6,9 @@ RSpec.describe Patient, type: :model do
   describe 'associations' do
     it { is_expected.to have_one(:account) }
     it { is_expected.to have_many(:appointments) }
-    it { is_expected.to have_many(:doctors).through(:appointments) }
+    it { is_expected.to have_many(:appointed_doctors).through(:appointments) }
+    it { is_expected.to have_many(:doctor_likes) }
+    it { is_expected.to have_many(:liked_doctors).through(:doctor_likes) }
   end
 
   describe 'delegations' do
