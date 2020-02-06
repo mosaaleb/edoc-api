78e6711a7226f156bada33b6d0ac675afb70ab79
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-06 03:35:32 +0200

    change validatioon error for uniqueness appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,5 +5,12 @@ class Appointment < ApplicationRecord
   belongs_to :patient
 
   validates :date, presence: true
-  validates :doctor, uniqueness: { scope: :patient_id }
+  validates :doctor,
+            uniqueness: {
+              scope: :patient_id,
+              message: lambda do |object, _|
+                "Can't have another appointment with Dr.#{object
+                .doctor.full_name}"
+              end
+            }
 end
