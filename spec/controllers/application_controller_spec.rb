76a6a46bc75ead91b15bb27469518e6887a10d0a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-02-11 21:43:24 +0200

    Fix application controller tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe ApplicationController, type: :controller do
   subject(:app) { described_class.new }
 
-  let!(:account) { create :account }
+  let(:account) { create(:account, :for_patient) }
   let(:headers) do
     {
       'Authorization' => "Bearer #{JsonWebToken.encode(account_id: account.id)}"
@@ -15,21 +15,22 @@ RSpec.describe ApplicationController, type: :controller do
 
   describe '#authorize_request' do
     context 'when auth token is present in headers' do
-      before { allow(request).to receive(:headers).and_return(headers) }
+      before { allow(app.request).to receive(:headers).and_return(headers) }
 
       it 'sets the current user' do
-        expect(app.send(:authorize_request)).to eq(account)
+        expect(app.send(:authorize_request))
+          .to eq(Patient.find(account.role_id))
       end
     end
 
     context 'when auth token is not passed' do
       before do
-        allow(request).to receive(:headers).and_return(invalid_headers)
+        allow(app.request).to receive(:headers).and_return(invalid_headers)
       end
 
       it 'raises MissingToken error' do
         expect { app.send(:authorize_request) }
-          .to raise_error(ExceptionHandler::MissingToken, /Missing token/)
+          .to raise_error(ExceptionHandler::MissingToken, /Missing Token/)
       end
     end
   end
