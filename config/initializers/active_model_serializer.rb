a48b6530135a1fccc9383fde907e3687efde9f0a
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-03-26 22:44:12 +0200

    define default host for routes

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,2 +1,6 @@
 ActiveModel::Serializer.config.adapter = :attributes
 ActiveModel::Serializer.config.key_transform = :camel_lower
+
+Rails.application.routes.default_url_options = {
+  host: 'localhost:3000'
+}
