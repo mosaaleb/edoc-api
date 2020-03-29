27978a8a98cd1aa5bb4605a1c62a59e13fceb7b6
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-03-29 02:39:17 +0200

    increase token expiry date

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,8 +4,8 @@ class JsonWebToken
   # secret to encode and decode token
   HMAC_SECRET = Rails.application.secret_key_base
 
-  def self.encode(payload, exp = 24.hours.from_now)
-    # set expiry to 24 hours from creation time
+  def self.encode(payload, exp = 1.week.from_now)
+    # set expiry to one week from creation time
     payload[:exp] = exp.to_i
     # sign token with application secret
     JWT.encode(payload, HMAC_SECRET)
